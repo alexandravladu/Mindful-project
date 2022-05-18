3f7aa819671b6fb3d6d13358b3f6349f31e114e9
alexandravladu <78971710+alexandravladu@users.noreply.github.com>
2022-05-18 17:22:23 +0100

    added everything

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function subscribe(){
+let name = prompt("What is your name?");
+let age = prompt("What is your age?");
+let email = prompt("What is your email address?");
+
+
+if(age >= 15){
+alert("Thanks for subcribing, " + name + ". Have a wonderful day!");
+} else {
+ alert("Thanks for subcribing, " + name + ". We will be in touch!");   
+}
+
+};
+
+
+let subscribeButton = document.querySelector("button");
+contactButton.addEventListener("click", subscribe);
+
