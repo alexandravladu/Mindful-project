a05e9fd0f4bf469b1d9d46f4e529b4338109c599
Scott Adams <scott_adams080291@hotmail.com>
2022-05-21 20:27:35 +0100

    refactor(mostly-linting): nothing major at all!

--- js/app.js
+++ js/app.js
@@ -1,18 +1,13 @@
-function subscribe(){
-let name = prompt("What is your name?");
-let age = prompt("What is your age?");
-let email = prompt("What is your email address?");
+function subscribe() {
+  let name = prompt("What is your name?");
+  let age = prompt("What is your age?");
 
-
-if(age >= 15){
-alert("Thanks for subcribing, " + name + ". Have a wonderful day!");
-} else {
- alert("Thanks for subcribing, " + name + ". We will be in touch!");   
+  if (age >= 15) {
+    alert("Thanks for subcribing, " + name + ". Have a wonderful day!");
+  } else {
+    alert("Thanks for subcribing, " + name + ". We will be in touch!");
+  }
 }
-
-};
-
 
 let subscribeButton = document.querySelector("button");
 contactButton.addEventListener("click", subscribe);
-
